cf739d0a6516ec44d43d0695e399e3bb57879fea
shalini09101998 <72515438+shalini09101998@users.noreply.github.com>
2020-10-31 13:55:28 +0530

    Count The number of set bits in number

---
+++ 01_Number_of_set_bits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int returnsetbits(int n){
+	int coun = 0;
+	while(n>0){
+		if(n&1)coun++;
+		n = n>>1;
+	}
+	return coun;
+}
+
+int main(){
+	int n = 32;
+	n = returnsetbits(n);
+	cout<<n<<endl;
+	return 0;
+}
