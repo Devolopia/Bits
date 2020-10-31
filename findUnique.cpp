41df38696d815c7f94c87c7e9600cf745c271238
shalini09101998 <72515438+shalini09101998@users.noreply.github.com>
2020-10-31 14:09:07 +0530

    Update findUnique.cpp

--- findUnique.cpp
+++ findUnique.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
 
+int returnunique(int* a,int n){
+  // xr is for store the xor of whole array
+	int xr = 0;
+	for(int i=0;i<n;i++){
+		xr = xr ^ a[i];
+	}
+	return xr;
+}
+
+int main(){
+	int n = 7;
+	int arr[7] = {1,2,3,3,4,1,2};
+	int t = returnunique(arr,n);
+	cout<<t<<endl;
+	return 0;
+}
